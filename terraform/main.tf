bca70d00903029316b5794253fdbc7457827ceb0
Brandon Tucker <brandontucker@comcast.net>
2026-03-26 21:48:00 -0400

    Day 1 complete - URL shortener creation endpoint working

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,3 +78,35 @@ resource "aws_lambda_function" "shorten_url" {
     aws_iam_role_policy.lambda_dynamodb_policy
   ]
 }
+
+resource "aws_apigatewayv2_api" "url_shortener_api" {
+  name          = "url-shortener-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "shorten_lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.url_shortener_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.shorten_url.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_shorten" {
+  api_id    = aws_apigatewayv2_api.url_shortener_api.id
+  route_key = "POST /shorten"
+  target    = "integrations/${aws_apigatewayv2_integration.shorten_lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.url_shortener_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "allow_apigw_invoke" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.shorten_url.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.url_shortener_api.execution_arn}/*/*"
+}
